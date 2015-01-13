091b56f427d6f9509e06aae6b94975d6618ace7c
Erik Axelsson <erikaxelsson1@gmail.com>
2015-01-13 01:29:02 +0100

    working real time box but whit bugs

--- app/views/hello.php
+++ app/views/hello.php
@@ -60,11 +60,27 @@
 		}
 	})(jQuery);
 
-
-
-	$( document ).ready(function() {
-
+	function init() {
 		oldText= $("#sqlBox").text();
+		myKey=Math.floor((Math.random() * 1000) + 1);
+		$("#myKey").html(myKey);
+
+		var historyNote={
+			"patch":[],
+			"userId":myKey,
+			"patchId":historyNumber
+		};
+		historyNumber=historyNumber+1;
+		changeHistory.push(historyNote);
+
+
+	}
+
+	$( document ).ready(function(){
+		init();
+
+
+
 		var a = document.getElementById('sqlBox');
 		a.onpaste = a.onchange = changed;
 		if ('oninput' in a) {
@@ -78,7 +94,7 @@
 			var newText=$("#sqlBox").val();
 			var diff = JsDiff["diffChars"](oldText,newText);
 
-
+			console.log(changeHistory);
 			var patchOn=changeHistory[changeHistory.length-1];
 
 			oldText=newText;
@@ -88,8 +104,8 @@
 				"from":myKey,
 				"nummber":historyNumber,
 				"patchOn":{
-					userId=patchOn.userId,
-					patchId=patchOn.patchId
+					"userId":patchOn.userId,
+					"patchId":patchOn.patchId
 				}
 			};
 			var historyNote={
@@ -109,8 +125,6 @@
 
 		};
 
-		myKey=Math.floor((Math.random() * 1000) + 1);
-		$("#myKey").html(myKey);
 
 
 		var peer = new Peer(myKey, {key: '9b406c3gm4jwcdi'});
@@ -151,7 +165,7 @@
 						"type":"fullHistory",
 						"history":changeHistory
 					};
-
+					connNewPeer.send(JSON.stringify(msg));
 
 
 
@@ -168,31 +182,15 @@
 				if(obj.type=="patch") {
 					console.log("get data");
 
-
-					console.log(obj);
 					doPatch(obj.patchs)
-					function doPatch(patchs) {
-						var text = $("#sqlBox").val();
-						var whereInLine = 0;
-						//var newText = "";
-						patchs.forEach(function (entry) {
-							console.log(entry);
-							if (entry.added) {
-								console.log("added");
-						//		newText += entry.value;
-							} else if (entry.removed) {
-								console.log("removed");
-								whereInLine += entry.count
-							} else {
-								console.log("unchanges");
-								//newText += entry.value;
-								whereInLine += entry.count
-							}
 
-						});
-						$("#sqlBox").val(newText);
 
-					}
+					var historyNote={
+						"patch":obj.patchs,
+						"userId":obj.from,
+						"patchId":obj.patchOn
+					};
+					changeHistory.push(historyNote);
 				}else if(obj.type=="updateClients"){
 					obj.clients.forEach(function(client){
 						var exist=false;
@@ -201,24 +199,51 @@
 								exist=true;
 							}
 						});
-						if(!exist){
+						if(!exist&&client!=myKey){
 							var connNewPeer = peer.connect(client);
 							clients.push(connNewPeer);
 						}
 					});
 				}else if(obj.type=="fullHistory"){
-					history=obj.history;
+					changeHistory=obj.history;
 
-					function runHistory(){
-
-
-					}
+					changeHistory.forEach(function(commit){
+						doPatch(commit.patch);
+					});
 				}
 			});
 
 		});
 
+	function doPatch(patchs) {
+		var text = $("#sqlBox").val();
+		var whereInLine = 0;
+		var newTextArr=[];
+		patchs.forEach(function (entry) {
+			console.log(entry);
+			if (entry.added) {
+				console.log("added");
+				console.log(entry.value);
+				newTextArr.push(entry.value);
+				//		newText += entry.value;
+			} else if (entry.removed) {
+				console.log("removed");
+				whereInLine += entry.count
+			} else {
+				console.log("unchanges");
+				newTextArr.push(text.substr(whereInLine,whereInLine+entry.count));
 
+				whereInLine += entry.count
+			}
+
+		});
+		newTextArr.push(text.substr(whereInLine,text.length));
+
+
+		oldText=newTextArr.join("");
+		$("#sqlBox").val(oldText);
+
+	}
 
 
 	$("#connectToRTC").click(
